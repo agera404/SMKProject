c188158f66fba3285d0b3d2e3b0c528655ea059c
Alina <agera404@gmail.com>
2020-11-11 15:32:57 +0500

    Создала структуру проекта. Добавила в проект MainView, MainPresenter, AddRecipeView, AddRecipePresenter, Recipe.

--- app/src/main/java/com/example/smkproject/MainActivity.kt
+++ app/src/main/java/com/example/smkproject/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.smkproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.smkproject.presenters.MainPresenter
+import com.example.smkproject.views.MainView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainView {
+    private val presenter = MainPresenter(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    override fun navigateTo(target: Class<*>) {
+        startActivity(Intent(this, target))
+    }
+
+    override fun onAddNewRecipe(view: View) {
+        presenter.onAddNewRecipe()
+    }
+
+
+
+
 }
